ab802f53f163e4da7d5f5696e298869ea4535370
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 15:04:00 +0530

    Added: Find Maximum Number

---
+++ src/main/java/com/blz/generics/Maximum.java
@@ -0,0 +1,17 @@
+package com.blz.generics;
+
+public class Maximum {
+    public static Integer findMaximum(Integer a, Integer b, Integer c){
+        Integer max = a;
+        if (b.compareTo(a)>0 && b.compareTo(c)>0){
+            max = b;
+        }
+        if (c.compareTo(max)>0){
+            max = c;
+        }
+        return max;
+    }
+    public static void main(String[] args){
+        System.out.println("Welcome To Generics Maximum Program");
+    }
+}
