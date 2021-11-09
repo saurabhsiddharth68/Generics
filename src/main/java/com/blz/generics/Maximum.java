461bf60fbb6b0bae432563c204cdc503e6f3b2ff
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 15:26:52 +0530

    Added: Find max Float Number

--- src/main/java/com/blz/generics/Maximum.java
+++ src/main/java/com/blz/generics/Maximum.java
@@ -2,20 +2,19 @@ package com.blz.generics;
 
 public class Maximum {
 
-    public static Integer findMaximum(Integer a, Integer b, Integer c){
-        Integer max = a;
-        if (b.compareTo(a)>0 && b.compareTo(c)>0){
+    public static Float findMaximum(Float a, Float b, Float c) {
+        Float max = a;
+        if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
             max = b;
         }
-        if (c.compareTo(max)>0){
+        if (c.compareTo(max) > 0) {
             max = c;
         }
-        System.out.println(max);
         return max;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 
         System.out.println("Welcome To Generics Maximum Program");
-        Maximum.findMaximum(6,3,8);
     }
 }
